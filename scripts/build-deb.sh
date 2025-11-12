c71973ffa268a7ac1305d30deb35f2bc67d1b74a
Siak Hooi <siakhooi@gmail.com>
2025-11-12 11:01:12 +0000

    fix build-deb.sh

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -9,13 +9,12 @@ readonly full_name=$COMPONENT_NAME-$RELEASE_VERSION
 readonly build_root=target
 readonly build_home=$build_root/$full_name
 readonly source_home=src
-readonly deb_file="${build_home}.deb"
 
 mkdir -p "$build_home"
 rm -rf "${build_home:?}"/*
 
 # Control File
-cp -vr $source_home/DEBIAN "$build_root"
+cp -vr $source_home/DEBIAN "$build_home"
 
 # Binary File
 readonly build_bin_home=$build_home/usr/bin
@@ -25,12 +24,14 @@ find $source_home/bin -type f -exec cp -vr {} "$build_bin_home" \;
 
 chmod 755 "$build_bin_home"/*
 
-fakeroot dpkg-deb --build -Zxz "$build_root"
-dpkg-name ${build_root}.deb
+fakeroot dpkg-deb --build -Zxz "$build_home"
+dpkg-name ${build_home}.deb
 
-DEBFILE=$(ls ./*.deb)
+DEBFILE=$(ls ./target/*.deb)
 
-sha256sum "$DEBFILE" >"$DEBFILE.sha256sum"
-sha512sum "$DEBFILE" >"$DEBFILE.sha512sum"
+mv -v "$DEBFILE" .
+deb_file=$(basename "$DEBFILE")
+sha256sum "$deb_file" >"$deb_file.sha256sum"
+sha512sum "$deb_file" >"$deb_file.sha512sum"
 
-dpkg --contents "$DEBFILE"
+dpkg --contents "$deb_file"
